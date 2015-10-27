d0a5c2ac3ec50c23e5cfc296699b9776fea3fe78
Richard Parayno <broodheart@gmail.com>
2015-10-27 18:05:32 +0800

    fixed gui! initial implementation of control panel buttons and map jlabels

--- mpMallKiosk/src/mpMallKiosk/AdminMenu.java
+++ mpMallKiosk/src/mpMallKiosk/AdminMenu.java
@@ -3,11 +3,7 @@ package mpMallKiosk;
 
 import java.awt.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class AdminMenu extends JFrame {
 
@@ -15,7 +11,6 @@ public class AdminMenu extends JFrame {
 	
 	private JPanel mapPanel;
 	private JPanel controlPanel;
-	private JPanel mainPanel;
 	private JButton controlButt1;
 	private JButton controlButt2;
 	private JButton controlButt3;
@@ -24,41 +19,39 @@ public class AdminMenu extends JFrame {
 	public AdminMenu() {
 		super("Mall Kiosk - Admin Menu");
 
-		mainPanel = new JPanel(new GridLayout(1,2));
-
+		setLayout(null);
 
 		//Creates the panel for the Map
 		mapPanel = new JPanel(new GridLayout(50,50,-1,-1));
 		mapPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		mapPanel.setSize(800, 700);
 
-
-		
-		
 		//Creates the panel for the controls
-		controlPanel = new JPanel(new GridLayout(10,10));
-
-		
+		controlPanel = new JPanel(new FlowLayout());
+		//Instantiates the buttons
 		controlButt1 = new JButton("Exit");
 		controlButt2 = new JButton("Edit");
 		controlButt3 = new JButton("Settings");
-	
+		//Adds the buttons
 		controlPanel.add(controlButt1);
 		controlPanel.add(controlButt2);
 		controlPanel.add(controlButt3);
-
-		controlPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-
+		//Sets the control panel's size and location on the frame
+		controlPanel.setSize(300,1000);
+		controlPanel.setLocation(800, 200);
+		//Sets the layout of the control panel
+		controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
 		//Creates the boxes
 		for (int i =0; i<(50*50); i++){
 			final JLabel box = new JLabel();
 		    box.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		    mapPanel.add(box);
 		}
+		//Adds the panels to the frames
+		add(mapPanel);
+		add(controlPanel);
 
-		mainPanel.add(mapPanel);
-		mainPanel.add(controlPanel);
-		add(mainPanel);
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1000,1000);
 		setVisible(true);
